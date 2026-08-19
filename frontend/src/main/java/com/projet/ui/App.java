732dbadff6d0f83eb0d5b3b4255c3da0f5673aa8
Smiley <smiley@kali.kali>
2026-08-19 14:23:52 +0300

    icon app

--- frontend/src/main/java/com/projet/ui/App.java
+++ frontend/src/main/java/com/projet/ui/App.java
@@ -3,7 +3,10 @@ package com.projet.ui;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.util.Objects;
 
 public class App extends Application {
 
@@ -12,6 +15,10 @@ public class App extends Application {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/main-layout.fxml"));
         Scene scene = new Scene(loader.load(), 1100, 700);
         scene.getStylesheets().add(getClass().getResource("/css/style.css").toExternalForm());
+
+        primaryStage.getIcons().add(
+            new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/bistro.png")))
+        );
 
         primaryStage.setTitle("BistroFlow");
         primaryStage.setScene(scene);
